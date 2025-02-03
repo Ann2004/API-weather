5d70e6410fb700a4bcc5b914ff1f4a7405b3e2cf
nyutdf2 <49587544+Ann2004@users.noreply.github.com>
2025-02-03 16:42:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
 
-url_template = 'https://wttr.in/{}'
 
-locations = ['London', 'svo', 'Череповец']
+def main():
+    url_template = 'https://wttr.in/{}'
 
-payload = {'MnqT': '', 'lang': 'ru'}
+    locations = [
+        'London', 
+        'svo', 
+        'Череповец'
+    ]
+    payload = {'MnqT': '', 'lang': 'ru'}
 
-for location in locations:
-    url = url_template.format(location)
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    print(response.text)
+    for location in locations:
+        url = url_template.format(location)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+
+if __name__ == '__main__':
+    main()
